8673e720424beea70345247bdbfb17d527d00a74
Kaloyan (KT) Tsvetkov <kaloyan@gmail.com>
2025-05-28 23:22:43 +0300

    fix favicons

--- include.index.php
+++ include.index.php
@@ -12,10 +12,10 @@
 
 <meta name="description" content="#RuseHack2015 &mdash; Първият хакатон в Русе!">
 
-<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-<link rel="manifest" href="/site.webmanifest">
+<link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png">
+<link rel="manifest" href="./site.webmanifest">
 
 <!-- <link rel="canonical" href="https://rusehack2015.kaloyan.info/"> -->
 
